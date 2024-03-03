44413e04341aabc367471ba567667076fd69382b
Victor Zheleznov <victorzheleznov@ya.ru>
2024-03-03 23:11:35 +0300

    Remove ANMR

--- metrics_sin.m
+++ metrics_sin.m
@@ -1,4 +1,4 @@
-% calculate SNR, NMR and ANMR for a nonlinearly processed sine signal
+% calculate SNR and NMR for a nonlinearly processed sine signal
 % input:
 %   sig --- test signal;
 %   fs --- sample rate [Hz];
@@ -8,9 +8,8 @@
 %   PLOT_SIG --- flag to plot input and bandlimited signals (optional).
 % output:
 %   snr --- signal-to-noise ratio;
-%   nmr --- noise-to-mask ratio;
-%   anmr --- A-weighted NMR.
-function [snr, nmr, anmr] = metrics_sin(sig, fs, f0, IS_SYM, varargin)
+%   nmr --- noise-to-mask ratio.
+function [snr, nmr] = metrics_sin(sig, fs, f0, IS_SYM, varargin)
     % bit truncation for NMR calculation
     bit = 24;
 
@@ -22,10 +21,4 @@ function [snr, nmr, anmr] = metrics_sin(sig, fs, f0, IS_SYM, varargin)
 
     % calculate NMR
     nmr = calc_nmr(sig, sig_lim, fs, bit);
-
-    % ANMR
-    [b,a] = adsgn(fs);
-    sigA = filter(b, a, sig);
-    sig_limA = filter(b, a, sig_lim);
-    anmr = calc_nmr(sigA, sig_limA, fs, bit);
 end
